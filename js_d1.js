cee3453145e9e74872987e51c5bd625124af2139
Aviv972 <carmi217@gmail.com>
2021-01-18 15:23:08 +0200

    dailyc

---
+++ js_d1.js
@@ -0,0 +1,53 @@
+   //Exercises//
+
+    5 + "34"
+    5 - "4"
+    10 % 5
+    5 % 10
+    "Java" + "Script"
+    " " + " "
+    " " + 0
+    true + true
+    true + false
+    false + true
+    false - true
+    3 - 4
+    "Bob" - "bill"
+
+var me = ["my","favorite","color","is","blue"];
+
+var me = me.join(" ");
+
+console.log(me);
+
+    //Daily Challenge//
+
+var fruits = ["Banana", "Apples", "Oranges", "Blueberries"];
+//fruits.shift();
+//fruits.sort();
+fruits[1] =""
+console.log(fruits);
+
+function fruits() {
+  fruits.shift();
+  fruits.sort();
+  
+}
+console.log(fruits);
+
+var fruits2 = ["Kiwi", "Oranges", "Blueberries"];
+fruits2.reverse();
+
+console.log(fruits2);
+
+// shift    // Remove the first element of the array
+
+// var family = ['Amit', 'Anna', 'Francisco'];
+
+// family.shift();
+
+// //console.log(family.shift());
+
+// console.log(family);
+
+
