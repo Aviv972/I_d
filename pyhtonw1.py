b431b51b745cbe6729d6c255c81e8d07833b6a6c
Aviv972 <carmi217@gmail.com>
2021-02-08 18:38:57 +0200

    pyw1

---
+++ pyhtonw1.py
@@ -0,0 +1,47 @@
+for i in range(4):
+    print("Hello World")
+
+
+number = (99^3) * 8
+print(number)
+
+computer_brand = "thinkpad"
+
+print("I have a razer computer" +" "+computer_brand)
+
+ 
+name = "Aviv"
+age = str(34)
+shoe_size = str(45)
+info = "Hi my name is" + " " + name +" "+ "and my age is" +" "+ age + " " + "I like shoes in the size of" + " " + shoe_size
+print(info)
+
+a = 5
+b = 6
+
+if a > b:
+  print("hello world")
+else:
+  print("fuck off")
+
+x = 3
+if x % 2 == 0:
+  print("==")
+else:
+  print("!=")
+
+user_name = "Tom"
+my_name = "Aviv"
+
+if user_name == my_name:
+  print("נולדת באביב?")
+else:
+  print("my name is cooler")
+
+
+Your_age = 75
+
+if (Your_age * 2.54) >= 145:
+  print("enjoy the ride")
+else:
+  print("go home!")
